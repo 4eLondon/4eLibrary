1e4ab5f672987e48d4c142199794c63ff0ca0bae
4eLondon <rhyncc@gmail.com>
2026-03-22 20:43:54 -0500

    Init: Created User Input Java file

---
+++ 1.programming/java/snippets/UserInput.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class UserInput {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Please enter your name: ");
+
+    String name = scanner.nextLine();
+    System.out.println("Hello " + name + "!");
+  }
+}
